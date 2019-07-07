fa63f1f045d32def76e09f0b3c4c5c8e2facf868
ocros03 <47435203+ocros03@users.noreply.github.com>
2019-07-07 19:30:14 -0400

    Update Restaurant.py

--- Restaurant.py
+++ Restaurant.py
@@ -1,12 +1,27 @@
 import random
+
 name = input("Who is using restaurant picker? ")
 
-rlist = ["RioGrande", \
-         "BurgerCo", \
-         "Easterbys", \
-         "OCharleys", \
-         "Habachihut"]
+rlist = ["Rio Chico", \
+         "Senor Tequila", \
+         "Easterby's", \
+         "O'Charley's", \
+         "Hibachi Hut"]
 
-rchoice=random.choice(rlist)
 
-print(name + ', I suggest '+ rchoice)
+
+while True:
+
+    rchoice=random.choice(rlist)
+    
+    print(name + ', I suggest '+ rchoice+ ' if you are happy with this choice press 1 if not press anything else')
+    
+    answer=input()
+    
+    if answer != '1':
+        continue 
+        
+    else:
+        print('Thank you and goodbye')
+        break
+        
